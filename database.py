fb1d2f400f62475f4eab9c7d7afa38b6e9abbc49
hridoy4147 <hridoyemon541@gmail.com>
2026-05-04 19:45:32 +0600

    Create database.py

---
+++ database.py
@@ -0,0 +1,11 @@
+import sqlite3
+
+def get_db():
+    conn = sqlite3.connect('hridoy_2fa.db', check_same_thread=False)
+    conn.row_factory = sqlite3.Row
+    return conn
+
+def init_db():
+    with get_db() as conn:
+        conn.execute('CREATE TABLE IF NOT EXISTS links (token TEXT PRIMARY KEY, account TEXT, secret TEXT, expiry REAL)')
+        conn.commit()
